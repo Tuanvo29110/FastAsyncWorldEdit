1427b8fa030a0c317aad05a03ac1156514c287b4
Tuanvo29110 <96452478+Tuanvo29110@users.noreply.github.com>
2025-07-16 01:28:33 +0700

    Update BukkitTaskManager.java

--- worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/util/BukkitTaskManager.java
+++ worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/util/BukkitTaskManager.java
@@ -1,53 +1,55 @@
 package com.fastasyncworldedit.bukkit.util;
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.fastasyncworldedit.core.util.TaskManager;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 
 import javax.annotation.Nonnull;
 
 public class BukkitTaskManager extends TaskManager {
 
-    private final Plugin plugin;
+    private final WorldEditPlugin plugin;
 
-    public BukkitTaskManager(final Plugin plugin) {
+    public BukkitTaskManager(final WorldEditPlugin plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public int repeat(@Nonnull final Runnable runnable, final int interval) {
-        return this.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(this.plugin, runnable, interval, interval);
+        return WorldEditPlugin.getScheduler().runTaskTimer(this.plugin, runnable, interval, interval);
     }
 
     @Override
     public int repeatAsync(@Nonnull final Runnable runnable, final int interval) {
-        return this.plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(this.plugin, runnable, interval, interval);
+        return WorldEditPlugin.getScheduler().runTaskTimerAsynchronously(this.plugin, runnable, interval, interval);
     }
 
     @Override
     public void async(@Nonnull final Runnable runnable) {
-        this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, runnable).getTaskId();
+        WorldEditPlugin.getScheduler().runTaskAsynchronously(this.plugin, runnable).getTaskId();
     }
 
     @Override
     public void task(@Nonnull final Runnable runnable) {
-        this.plugin.getServer().getScheduler().runTask(this.plugin, runnable).getTaskId();
+        WorldEditPlugin.getScheduler().runTask(this.plugin, runnable).getTaskId();
     }
 
     @Override
     public void later(@Nonnull final Runnable runnable, final int delay) {
-        this.plugin.getServer().getScheduler().runTaskLater(this.plugin, runnable, delay).getTaskId();
+        WorldEditPlugin.getScheduler().runTaskLater(this.plugin, runnable, delay).getTaskId();
     }
 
     @Override
     public void laterAsync(@Nonnull final Runnable runnable, final int delay) {
-        this.plugin.getServer().getScheduler().runTaskLaterAsynchronously(this.plugin, runnable, delay);
+        WorldEditPlugin.getScheduler().runTaskLaterAsynchronously(this.plugin, runnable, delay);
     }
 
     @Override
     public void cancel(final int task) {
-        if (task != -1) {
+        try {
             Bukkit.getScheduler().cancelTask(task);
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.WARNING, "Error cancelling FAWE task in Folia", e);
         }
     }
 
